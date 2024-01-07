8423ee7bd214fb90a9f8ee86ea985488d4735fb1
MD HARUNUR RASHID <35065199+Harun1302123@users.noreply.github.com>
2024-01-07 19:37:49 +0600

    Update mergeAlternately.php

--- mergeAlternately.php
+++ mergeAlternately.php
@@ -1,23 +1,15 @@
 class Solution {
 
     /**
-    * @param String $word1
-    * @param String $word2
-    * @return String
-    */
+     * @param String $word1
+     * @param String $word2
+     * @return String
+     */
     function mergeAlternately($word1, $word2) {
-        $i = 0;
-        $output = '';
-        while (isset($word1[$i]) || isset($word2[$i])) {
-
-            if (isset($word1[$i])) {
-                $output .= $word1[$i];
-            }
-            if (isset($word2[$i])) {
-                $output .= $word2[$i];
-            }
-
-            $i++;
-        }
+        $str = [];
+        for($i = 0; $i < max(strlen($word1), strlen($word2)); $i++)
+            $str[] = $word1[$i] . $word2[$i];
+            
+        return implode("", $str);
     }
 }
